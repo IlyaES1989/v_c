4253c959c7116c45e0144ae2ec09d5d532d1334e
Ilya Stromov <istromov@neuro.net>
2022-01-17 22:08:37 +0500

    simple docker conf

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import redis
+
+
+LIB_VERSION ='2.0.2'
+
+if LIB_VERSION:
+    sys.path.append(f'/custom_folder/flask/{LIB_VERSION}')
+    import flask
+
+
+def main():
+    print('Run main')
+    print(f'Pillow version {redis.__version__}')
+
+    print(f'flask version {flask.__version__}')
+
+
+if __name__ == "__main__":
+    main()
